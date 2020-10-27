712c0ccd002feec5d79ed5b7379e397892ad5882
NullCub3 <tigercraft3k@gmail.com>
2020-10-27 11:01:37 -0700

    Hello Command + Console Printing

--- main.py
+++ main.py
@@ -2,21 +2,25 @@
 
 import discord
 import time
+import os
 
 #I just imported stuff you big dum-dum
 
-client = discord.client()
+client = discord.Client()
 
+#This prints a message in the console saying the bot is connected
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
 @client.event
 async def on_message(message):
+    print(f'Message from {message.author}: {message.content}')
     if message.author == client.user:
         return
     
     if message.content.startswith('^hello'):
+        print('Saying Hello')
         await message.channel.send('Hello!')
 
-client.run(os.envioron['BOT_SECRET'])
+client.run(os.environ['BOT_SECRET'])
