488128f2f0aff6b2236d86cb0586f0cccab99fda
NullCub3 <tigercraft3k@gmail.com>
2020-10-29 16:30:36 -0700

    Added some commands that don't work

--- main.py
+++ main.py
@@ -16,21 +16,26 @@ logging.basicConfig(level=logging.INFO)
 client = discord.Client()
 
 #Defining Discord Commands
-
+bot = commands.Bot(command_prefix='^')
 
 #This prints a message in the console saying the bot is connected
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    print(f'Message from {message.author}: {message.content}')
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('^hello'):
-        print('Saying Hello')
-        await message.channel.send('Hello!')
+#Maybe prints hello?
+@bot.command()
+async def hello(ctx):
+    print('Saying Hello')
+    ctx.send(f'Hello {ctx.author}!')
+
+@bot.command()
+async def echo(ctx, arg):
+    print('echoing')
+    ctx.send(arg)
+
+@bot.command()
+async def test(ctx):
+    pass
 
 client.run(os.environ['BOT_SECRET'])
